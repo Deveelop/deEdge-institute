3c8eed282afcd9beabc80d689ffe8b0455cb5506
vicson <vicsonabuka@gmail.com>
2023-11-29 21:55:12 +0100

    Third commit

---
+++ src/Components/testimonyData.jsx
@@ -0,0 +1,24 @@
+export const TestimonyData = [
+    {
+        id:'a1',
+        student: 'Vicson Nathaniels',
+        studentLocation: 'India',
+        comment: ' Having to earn in naira as a self employed young man got really difficult as cost of living went on the hike due to the fall of naira to dollar. Fast forward to the month of September when Sir Peter introduced me to deEDGE Institute , my financial life has got better and spendig got flexible because of the skills i acquired from this platform. Thank God for deEDGE Institute',
+        url:'/color1.png'
+    },
+    {
+        id:'a2',
+        student: 'Benaya Daniels',
+        studentLocation: 'Nasarawa',
+        comment: ' Having to earn in naira as a self employed young man got really difficult as cost of living went on the hike due to the fall of naira to dollar. Fast forward to the month of September when Sir Peter introduced me to deEDGE Institute , my financial life has got better and spendig got flexible because of the skills i acquired from this platform. Thank God for deEDGE Institute',
+        url:'/color1.png'
+    },
+    {
+        id:'a3',
+        student: 'Peter A.A',
+        studentLocation: 'Makurdi',
+        comment: ' Having to earn in naira as a self employed young man got really difficult as cost of living went on the hike due to the fall of naira to dollar. Fast forward to the month of September when Sir Peter introduced me to deEDGE Institute , my financial life has got better and spendig got flexible because of the skills i acquired from this platform. Thank God for deEDGE Institute',
+        url:'/color1.png'
+    },
+
+]
--- src/TemplateDesign.jsx
+++ src/TemplateDesign.jsx
@@ -1,3 +1,11 @@
+import React from "react";
+import { Button, Collapse } from "react-bootstrap";
+import {FaBars, FaSignal, FaMapMarker ,FaGraduationCap, FaBullhorn, FaEnvira, FaGlobe, FaGlobeAfrica } from 'react-icons/fa'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { TestimonyData } from "./Components/testimonyData";
+
 const TemplateDesign = () => {
     return ( 
     <div id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
@@ -5,13 +13,10 @@ const TemplateDesign = () => {
 <nav className="navbar navbar-default navbar-fixed-top">
   <div className="container">
     <div className="navbar-header">
-      <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        {/* <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>                         */}
-        Toggle
+      <button type="button">
+        <FaBars/>               
       </button>
-      <a class="navbar-brand" href="#">
+      <a className="navbar-brand" href="#">
       <img src="/company_logo.png" alt="company logo" width="50" height="50"/>
     </a>
     </div>
@@ -61,7 +66,7 @@ Discover how to unlock the potential of your smartphone, your financial resource
       <button className="btn btn-default btn-lg">Get in Touch</button>
     </div>
     <div className="col-sm-4">
-      <span className="glyphicon glyphicon-signal logo"></span>
+      <FaSignal size='15rem' color="#FF914D"/>
     </div>
   </div>
 </div>
@@ -69,7 +74,7 @@ Discover how to unlock the potential of your smartphone, your financial resource
 <div className="container-fluid bg-grey">
   <div className="row">
     <div className="col-sm-4">
-      <span className="glyphicon glyphicon-globe logo slideanim"></span>
+      <FaGlobeAfrica className="slide" size='15rem' color="#FF914D" />
     </div>
     <div className="col-sm-8">
       <h2>Our Values</h2><br/>
@@ -91,48 +96,170 @@ Discover how to unlock the potential of your smartphone, your financial resource
   <br/>
   <div className="row slideanim">
     <div className="col-sm-4">
-      <span className="glyphicon glyphicon-certificate logo-small"></span>
+      <FaGraduationCap  size='5rem' color="#FF914D"/>
       <h4>CERTIFIED COURSES</h4>
       <p>We offer Courses that can help you build profitable digital skills and implement them with just your Smart phone. Which you can use to work online and also setup your online businesses.</p>
     </div>
     <div className="col-sm-4">
-      <span className="glyphicon glyphicon-wrench logo-small"></span>
+      <FaBullhorn  size='5rem' color="#FF914D"/>
       <h4>AFFILIATE MARKETING</h4>
       <p>Lorem ipsum dolor sit amet..</p>
     </div>
   </div>
 </div>
 
-<div id="portfolio" class="container-fluid text-center bg-grey">
+<div id="portfolio" className="container-fluid text-center bg-grey">
   <h2>Portfolio</h2><br/>
   <h4>What we have created</h4>
-  <div class="row text-center slideanim">
-    <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="paris.jpg" alt="Sproutgigs" width="400" height="300"/>
+  <div className="row text-center slideanim">
+    <div className="col-sm-4">
+      <div className="thumbnail">
+        <FaEnvira/>
         <p><strong>Sproutgigs Freelancer Guide</strong></p>
         <p>Yes, we built an enriched tutorial on how to earn swiftly with just your mobile phone as a Freelancer on Sproutgigs platform.</p>
       </div>
     </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
+    <div className="col-sm-4">
+      <div className="thumbnail">
         <img src="newyork.jpg" alt="New York" width="400" height="300"/>
         <p><strong>New York</strong></p>
         <p>We built New York</p>
       </div>
     </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
+    <div className="col-sm-4">
+      <div className="thumbnail">
         <img src="sanfran.jpg" alt="San Francisco" width="400" height="300"/>
         <p><strong>San Francisco</strong></p>
         <p>Yes, San Fran is ours</p>
       </div>
     </div>
   </div>
+</div><br/>
+
+<div className=" text-center bg-grey px-0 py-4">
+<div className="">
+<h2 style={{fontSize: '15px'}}>what our students say</h2>
+    <Row xs={1} md={3} className="g-4 m-2">
+      {TestimonyData.map((idx) => (
+        <Col key={idx.id}>
+          <Card style={{ width: '18rem' }} >
+      <Card.Img variant="top" src={idx.url} />
+      <Card.Body >
+        <Card.Title >{idx.student}</Card.Title>
+        <Card.Text style={{fontSize: '10px'}}  >
+         {idx.comment}
+        </Card.Text>
+        <Card.Text>
+        <label ><span><FaMapMarker/></span>{idx.studentLocation}</label>
+        </Card.Text>
+        
+        <Button variant="primary">See earnings</Button>
+      </Card.Body>
+    </Card>
+        </Col>
+      ))}
+    </Row>
+    </div>
+    </div>
+    <div id="pricing" className="container-fluid">
+  <div className="text-center">
+    <h2>Pricing</h2>
+    <h4>Choose a payment plan that works for you</h4>
+  </div>
+  <div className="row slideanim">
+    <div className="col-sm-4 col-xs-12">
+      <div className="panel panel-default text-center">
+        <div className="panel-heading">
+          <h1>Basic</h1>
+        </div>
+        <div className="panel-body">
+          <p><strong>20</strong> Lorem</p>
+          <p><strong>15</strong> Ipsum</p>
+          <p><strong>5</strong> Dolor</p>
+          <p><strong>2</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+        </div>
+        <div className="panel-footer">
+          <h3>$19</h3>
+          <h4>per month</h4>
+          <button className="btn btn-lg">Sign Up</button>
+        </div>
+      </div>      
+    </div>     
+    <div className="col-sm-4 col-xs-12">
+      <div className="panel panel-default text-center">
+        <div className="panel-heading">
+          <h1>Pro</h1>
+        </div>
+        <div className="panel-body">
+          <p><strong>50</strong> Lorem</p>
+          <p><strong>25</strong> Ipsum</p>
+          <p><strong>10</strong> Dolor</p>
+          <p><strong>5</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+        </div>
+        <div className="panel-footer">
+          <h3>$29</h3>
+          <h4>per month</h4>
+          <button className="btn btn-lg">Sign Up</button>
+        </div>
+      </div>      
+    </div>       
+    <div className="col-sm-4 col-xs-12">
+      <div className="panel panel-default text-center">
+        <div className="panel-heading">
+          <h1>Premium</h1>
+        </div>
+        <div className="panel-body">
+          <p><strong>100</strong> Lorem</p>
+          <p><strong>50</strong> Ipsum</p>
+          <p><strong>25</strong> Dolor</p>
+          <p><strong>10</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+        </div>
+        <div className="panel-footer">
+          <h3>$49</h3>
+          <h4>per month</h4>
+          <button className="btn btn-lg">Sign Up</button>
+        </div>
+      </div>      
+    </div>    
+  </div>
 </div>
+<div id="contact" class="container-fluid bg-grey">
+  <h2 className="text-center">CONTACT</h2>
+  <div className="row">
+    <div className="col-sm-5">
+      <p>Contact us and we'll get back to you within 24 hours.</p>
+      <p><span className="glyphicon glyphicon-map-marker"></span>NO. 5 Udik Villa Ichwa North Bank, Makurdi Benue State.</p>
+      <p><span className="glyphicon glyphicon-phone"></span> +2349033317004</p>
+      <p><span className="glyphicon glyphicon-phone"></span> +2348065032361</p>
+      <p><span className="glyphicon glyphicon-phone"></span> +2349033988566</p>
+      <p><span className="glyphicon glyphicon-envelope"></span> dedgeinstitute@gmail.com</p>
+    </div>
+    <div className="col-sm-7 slideanim">
+      <div className="row">
+        <div className="col-sm-6 form-group">
+          <input className="form-control" id="name" name="name" placeholder="Name" type="text" required/>
+        </div>
+        <div className="col-sm-6 form-group">
+          <input className="form-control" id="email" name="email" placeholder="Email" type="email" required/>
+        </div>
+      </div>
+      <textarea className="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br/>
+      <div className="row">
+        <div className="col-sm-12 form-group">
+          <button className="btn btn-default pull-right" type="submit">Send</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 </div>
 )
 }
 
-export default TemplateDesign;
-
+export default TemplateDesign
